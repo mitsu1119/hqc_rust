e96bed974ae1e4dc201f26eeed8730bf5e79a9fe
mitsu1119 <hachimitsu1119@gmail.com>
2025-09-29 17:59:38 +0900

    refactor

--- src/hqc/hqc_hash.rs
+++ src/hqc/hqc_hash.rs
@@ -1,19 +1,53 @@
-use sha3::{Digest, Sha3_512};
+use sha3::{Digest, Sha3_256, Sha3_512};
 
 pub struct HQCHash {}
 
 impl HQCHash {
-    const HQC_G_DOMAIN: [u8; 1] = [0];
-    const HQC_H_DOMAIN: [u8; 1] = [1];
-    const HQC_I_DOMAIN: [u8; 1] = [2];
-    const HQC_J_DOMAIN: [u8; 1] = [3];
+    const HQC_G_DOMAIN: u8 = 0;
+    const HQC_H_DOMAIN: u8 = 1;
+    const HQC_I_DOMAIN: u8 = 2;
+    const HQC_J_DOMAIN: u8 = 3;
 
-    pub fn G(str: &[u8]) -> [u8; 64] {
+    #[allow(non_snake_case)]
+    pub fn G(data: &[u8]) -> [u8; 64] {
         let mut h = Sha3_512::new();
         h.update(str);
-        h.update(&Self::HQC_G_DOMAIN);
+        h.update(&[Self::HQC_G_DOMAIN]);
         let out = h.finalize();
         let mut res = [0u8; 64];
+        res.copy_from_slice(&out);
+        res
+    }
+
+    #[allow(non_snake_case)]
+    pub fn I(data: &[u8]) -> [u8; 64] {
+        let mut h = Sha3_512::new();
+        h.update(str);
+        h.update(&[Self::HQC_I_DOMAIN]);
+        let out = h.finalize();
+        let mut res = [0u8; 64];
+        res.copy_from_slice(&out);
+        res
+    }
+
+    #[allow(non_snake_case)]
+    pub fn H(data: &[u8]) -> [u8; 32] {
+        let mut h = Sha3_256::new();
+        h.update(str);
+        h.update(&[Self::HQC_H_DOMAIN]);
+        let out = h.finalize();
+        let mut res = [0u8; 32];
+        res.copy_from_slice(&out);
+        res
+    }
+
+    #[allow(non_snake_case)]
+    pub fn J(data: &[u8]) -> [u8; 32] {
+        let mut h = Sha3_256::new();
+        h.update(str);
+        h.update(&[Self::HQC_J_DOMAIN]);
+        let out = h.finalize();
+        let mut res = [0u8; 32];
         res.copy_from_slice(&out);
         res
     }
