2bf401acbb4e76ddb1b2d6fa84a8a5604412cac3
mitsu1119 <hachimitsu1119@gmail.com>
2025-10-09 21:12:08 +0900

    hqc.rsのテストをKATParserで書き直した

--- src/util/kat_parser.rs
+++ src/util/kat_parser.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader, Error},
+    io::{self, BufRead, BufReader},
 };
 
 #[derive(Debug)]
@@ -15,7 +15,7 @@ impl<'a> KATParser {
         Ok(Self { br })
     }
 
-    fn line_after(&mut self, s: &'a str) -> io::Result<Option<String>> {
+    pub fn line_after(&mut self, s: &'a str) -> io::Result<Option<String>> {
         let mut res = String::new();
 
         loop {
