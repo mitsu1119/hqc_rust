47810d4ccba26161d6d1c6c2d4ee3e3d1f2c6831
mitsu1119 <hachimitsu1119@gmail.com>
2025-10-09 16:45:43 +0900

    xofの実装を公式に合わせて修正

--- src/hqc/xof.rs
+++ src/hqc/xof.rs
@@ -8,18 +8,19 @@ pub struct XOF {
 }
 
 impl XOF {
+    const HQC_XOF_DOMAIN: u8 = 1;
+
     pub fn new(seed: &[u8]) -> Self {
         let mut ctx = Shake256::default();
         ctx.update(seed);
+        ctx.update(&[Self::HQC_XOF_DOMAIN]);
         Self {
             reader: ctx.finalize_xof(),
         }
     }
 
     pub fn init(&mut self, seed: &[u8]) {
-        let mut ctx = Shake256::default();
-        ctx.update(&seed);
-        self.reader = ctx.finalize_xof();
+        *self = Self::new(seed);
     }
 
     fn squeeze_into(&mut self, out: &mut [u8]) {
@@ -29,6 +30,11 @@ impl XOF {
     pub fn get_bytes(&mut self, len: usize) -> Vec<u8> {
         let mut out = vec![0u8; len];
         self.squeeze_into(&mut out);
+
+        // 公式のxof実装に合わせて追加で消費する
+        let mut _trash = vec![0u8; (8 - len % 8) as usize];
+        self.squeeze_into(&mut _trash);
+
         out
     }
 }
